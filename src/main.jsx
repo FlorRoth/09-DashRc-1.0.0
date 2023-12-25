a5c54ce8390c3bcfef4ccff033dfea6c9da69ef6
FlorRoth <roth.florencia99@gmail.com>
2023-12-25 19:48:10 -0300

    add auth provider in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthPovider } from './providers/AuthPovider.jsx';
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode>
-     {/* <RouterProvider router={router} /> */}
-     <App />
+     <AuthPovider>
+       <App />
+     </AuthPovider>
   </React.StrictMode>,
 )
